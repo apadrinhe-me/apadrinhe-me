3754541f5fc2b96a7fc18dad0d5ceda8e22ce44e
Natan Barbosa <natanbarbosa525@gmail.com>
2022-05-19 01:36:32 -0300

    componente post finalizado. alguns problemas de layout corrigidos

--- src/componets/layout/post/Post.jsx
+++ src/componets/layout/post/Post.jsx
@@ -3,6 +3,14 @@ import React from "react";
 import ProfPicture from "../profPicture/ProfPicture";
 import getPostDate from "../../funcional/getPostDate";
 
+//import dos ícones
+import likeIcoFill from "../../../media/icons/item_like_icon_fill.svg";
+import likeIcoStroke from "../../../media/icons/item_like_icon_stroke.svg";
+import commentIcoFill from "../../../media/icons/item_comment_icon_fill.svg";
+import commentIcoStroke from "../../../media/icons/item_comment_icon_stroke.svg";
+import shareIco from "../../../media/icons/item_share_icon_stroke.svg";
+import rocketIco from "../../../media/icons/icons_apadrinhar_icon_fill.svg";
+
 const Post = props => {
     //formatar horáario do post
 
@@ -17,6 +25,23 @@ const Post = props => {
                     <span className="aspiration">{props.aspiration}</span>
                     <span className="postDate">{getPostDate(props.postDate)}</span>
                 </div>
+            </div>
+            <div className="post-description">
+                {props.postDescription}
+            </div>
+            <div className="post-media">
+                <img src={require(`../../../media/post_media/${props.postMedia}`)} alt="Imagem do post" className="post-img"/>
+            </div>
+            <div className="post-engagement">
+                <span className="likes">{props.qntLikes} likes</span> <span className="separatos">-</span>
+                <span className="comments">{props.qntComments} comentários</span> <span className="separatos">-</span>
+                <span className="shared">{props.qntShares} compartilhamentos</span>
+            </div>
+            <div className="post-actionBtns">
+                <button className="btn btn-like"> <img src={likeIcoStroke} alt="icone de like" className="btn-ico"/> Curtir</button>
+                <button className="btn btn-comment"><img src={commentIcoStroke} alt="icone de like" className="btn-ico"/> Comentar</button>
+                <button className="btn btn-share"><img src={shareIco} alt="icone de like" className="btn-ico"/> Compartilhar</button>
+                <button className="btn btn-apadrinhar"><img src={rocketIco} alt="icone de like" className="btn-ico"/>  Apadrinhar</button>
             </div>
         </div>
     );
--- src/views/pages/home.jsx
+++ src/views/pages/home.jsx
@@ -1,20 +1,70 @@
 import "./Home.css"
-import React from "react";
+import React, {useState} from "react";
 import Menu from "../../componets/layout/Menu";
 import Post from "../../componets/layout/post/Post";
+import If from "../../componets/funcional/If";
 
-const Home =  () => {
+const Home = () => {
+    let mobileView = window.innerWidth <= 576 ? true : false;
+    let desktopView = window.innerWidth > 576 ? true : false;
+
+    const[viewport, setviewport] = useState(window.innerWidth < 576 ? 'mobile' : 'desktop')
+
+    window.addEventListener("resize", () => {
+        if(window.innerWidth < 576){
+            setviewport('mobile')
+        } else {
+            setviewport('desktop')
+        }
+    })
+
     return(
         <>
             <Menu />
             <div className="Home">
-                <div className="infinity-post-scroll">
-                    <Post profPicture="natan_proa.jpg" fullname="Natan Barbosa" aspiration="Aspirante a desenvolvedor fullstack" postDate="2022-04-19 12:30:25" />
-                </div>
+                <If test={viewport === 'desktop'}>
+                    <div className="infinity-post-scroll">
+                        <div className="new-post">
+                            a
+                        </div>
 
-                <div className="spotlights">
-                    destaques
-                </div>
+                        <Post 
+                        profPicture="natan_proa.jpg" 
+                        fullname="Natan Barbosa" 
+                        aspiration="Aspirante a desenvolvedor fullstack" 
+                        postDate="2022-04-19 12:30:25"
+                        postDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio ducimus numquam quas fuga autem at amet cumque sint modi impedit esse asperiores veritatis, ullam itaque reiciendis quaerat perferendis cum."
+                        postMedia="mao_casa.jpg" 
+                        qntLikes={1000}
+                        qntComments={200}
+                        qntShares={10}/>
+                    </div>
+
+                    <div className="spotlights">
+                        destaques
+                    </div>
+                </If>
+
+                <If test={viewport === 'mobile'}>
+                    <div className="new-post">
+                        a
+                    </div>
+                    <div className="spotlights">
+                        destaques
+                    </div>
+                    <div className="infinity-post-scroll">
+                        <Post 
+                            profPicture="natan_proa.jpg" 
+                            fullname="Natan Barbosa" 
+                            aspiration="Aspirante a desenvolvedor fullstack" 
+                            postDate="2022-04-19 12:30:25"
+                            postDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio ducimus numquam quas fuga autem at amet cumque sint modi impedit esse asperiores veritatis, ullam itaque reiciendis quaerat perferendis cum."
+                            postMedia="mao_casa.jpg" 
+                            qntLikes={1000}
+                            qntComments={200}
+                            qntShares={10}/>
+                    </div>
+                </If>
             </div>
         </>
     );
