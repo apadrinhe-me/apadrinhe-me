3409acceafbda08d64a47ecdb7a49f212e91b471
Cristian ferreira <cristianferreiradeoliveira.ti@gmail.com>
2022-05-28 21:10:04 -0300

    cursos iniciada

---
+++ src/componets/layout/cardCursos/CardCurso.jsx
@@ -0,0 +1,41 @@
+import "./cardCurso.css";
+import React from "react";
+
+const CardCurso = props => {
+    return (
+        <>
+            <div className="cardCurso">
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                        <h3>Metodologia ágeis</h3>
+                        <h4>Intituto Proa</h4>
+                        <img src="" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In rerum,
+                            nesciunt repudiandae perferendis aut consectetur facere. Eius obcaecati
+                            vero maiores quisquam, assumenda ex fugiat consequatur quod delectus odio
+                            sunt officiis.</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p></p>
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+
+                    <div>
+                        <button></button>
+                        <button></button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore iusto molestiae eum 
+                        vero non fugiat sequi delectus? Quasi mollitia, expedita earum, ducimus magni, 
+                        officia odio voluptates nostrum laudantium sunt natus!</p>
+                </div>
+            </div>
+
+        </>
+
+    );
+}
+
+export default CardCurso;
--- src/views/pages/Cursos.jsx
+++ src/views/pages/Cursos.jsx
@@ -1,16 +1,57 @@
 import "./Cursos.css";
+
 import React from "react";
 import Menu from "../../componets/layout/Menu";
+import Cursos from "../../componets/layout/cardCursos/CardCurso"
+import administracaoicone from "../../media/icons/item_people_icon_stroke.svg"
 
-const Cursos =  () => {
-    return(
+const Curso = () => {
+    return (
         <>
             <Menu />
             <div className="Cursos">
-                <h1>Cursos</h1>
+
+                <div className="categoriasCurso">
+                    <ul>
+                        <li>
+
+                            <p>lore Todos os Cursos</p>
+                        </li>
+                        <li>    Administração</li>
+                        <li>  Tecnologia</li>
+                        <li>  Contabilidade e Finanças</li>
+                        <li>  Desenvolvimento Pessoal</li>
+                        <li>  Educação</li>
+                        <li>  Fundação TeC</li>
+                        <li>  Favoritos</li>
+                    </ul>
+                </div>
+
+                <div className="cardsCursos">
+
+                    <div>
+                        <h2>Todos os Cursos</h2>
+                        <button><img src="" alt="" /></button>
+                    </div>
+                    <div className="cardcurso" >
+
+                        <Cursos />
+                        <Cursos />
+                        <Cursos />
+                    </div>
+
+                </div>
+
+
             </div>
         </>
     );
 }
 
-export default Cursos;
+export default Curso;
+
+
+
+
+
+
