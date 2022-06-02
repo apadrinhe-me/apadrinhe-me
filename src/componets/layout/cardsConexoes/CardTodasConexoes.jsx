c2ad36070f22891acce33f0522f5f6599b2b4f69
Natan Barbosa <nerenatan@gmail.com>
2022-06-02 17:07:45 -0300

    conflitos solucionados

--- src/componets/layout/cardsConexoes/CardTodasConexoes.jsx
+++ src/componets/layout/cardsConexoes/CardTodasConexoes.jsx
@@ -5,8 +5,10 @@ import React from "react";
 const CardTodasConexoes = props => {
     return (
         <div className="cardUsuarioInteracao ">
-                    <button className="buttonUsuarioInteracao">Ver Perfil</button>
-                    <i class="bi bi-check-lg CardUsusarioInteracaoIcons"></i>
+            <div>
+                <button className="buttonUsuarioInteracao">Ver Perfil</button>
+                <i className="bi bi-check-lg CardUsusarioInteracaoIcons"></i>
+            </div>
         </div>
     );
 }
--- src/componets/layout/profileSubViews/mapaCarreira/editarMapaCarreira/EditorMapaCarreira.jsx
+++ src/componets/layout/profileSubViews/mapaCarreira/editarMapaCarreira/EditorMapaCarreira.jsx
@@ -6,19 +6,89 @@ import MenuMobile from "../../../MenuMobile";
 //imports das bibliotecas e estilos
 import "./EditorMapaCarreira.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 //imports das ilustrações
 import ill1 from "../../../../../media/illustrations/Progress_Flatline.svg";
 import ill2 from "../../../../../media/illustrations/Winner_Isometric.svg";
 import ill3 from "../../../../../media/illustrations/Motivation_Flatline.svg";
+import {Link} from "react-router-dom";
+import Timeline from "../Timeline";
 
 const EditorMapaCarreira = props => {
+    let goals = [
+        {
+            nome: "Goal 1",
+            year: 2015,
+            status: "achieved",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        },
+        {
+            nome: "Goal 2",
+            year: 2016,
+            status: "in-progress",
+            selected: false
+        }
+    ]
+
     return(
         <>
             <Menu/>
             <div className="EditorMapaCarreira">
                 <div className="explicacaoMapa my-scrollbar">
-                    <button className="close-btn"><i class="bi bi-x-lg"></i></button>
+                    <button className="close-btn"><i className="bi bi-x-lg"></i></button>
                     <h2 className="section-title">Editar Mapa de carreira</h2>
 
                     <div className="img-text">
@@ -49,7 +119,140 @@ const EditorMapaCarreira = props => {
                 </div>
 
                 <div className="montaMapa">
-                    
+                    <div className="header">
+                        <h3 className="section-title">Metas adicionadas</h3>
+                        <Link to="/perfil/mapacarreira/"><button className="return-profile"><i className="bi bi-arrow-return-left"></i> Voltar para o perfil</button></Link>
+                    </div>
+
+                    <div className="meta-cards">
+                        <Card sx={{ flexBasis: 200 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Meta escolar
+                                </Typography>
+                                <Typography variant="h3" sx={{ fontSize: 18 }}>
+                                    Completar o Ensino Médio
+                                </Typography>
+                                <div className="card-details">
+                                    <div className="detail">
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Ano:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            2016
+                                        </Typography>
+                                    </div>
+
+                                    <div className="detail">
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Área:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            Ensino Médio
+                                        </Typography>
+                                    </div>
+
+                                    <div className="detail">
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Instituição:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            SESI
+                                        </Typography>
+                                    </div>
+
+                                    <div>
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Detalhes:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            Finalmente me formar para...
+                                        </Typography>
+                                    </div>
+
+                                </div>
+                            </CardContent>
+                            <CardActions>
+                                <Button className="btn-editar" variant="outlined" size="small"><i className="bi bi-pencil-square"></i> Editar meta</Button>
+                            </CardActions>
+                        </Card>
+
+                        <Card sx={{ flexBasis: 200 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Meta escolar
+                                </Typography>
+                                <Typography variant="h3" sx={{ fontSize: 18 }}>
+                                    Completar o Ensino Médio
+                                </Typography>
+                                <div className="card-details">
+                                    <div className="detail">
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Ano:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            2016
+                                        </Typography>
+                                    </div>
+
+                                    <div className="detail">
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Área:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            Ensino Médio
+                                        </Typography>
+                                    </div>
+
+                                    <div className="detail">
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Instituição:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            SESI
+                                        </Typography>
+                                    </div>
+
+                                    <div>
+                                        <Typography sx={{ fontSize: 15, fontWeight: 700 }}>
+                                            Detalhes:
+                                        </Typography>
+                                        <Typography sx={{ fontSize: 15}}>
+                                            Finalmente me formar para...
+                                        </Typography>
+                                    </div>
+
+                                </div>
+                            </CardContent>
+                            <CardActions>
+                                <Button className="btn-editar" variant="outlined" size="small"><i className="bi bi-pencil-square"></i> Editar meta</Button>
+                            </CardActions>
+                        </Card>
+
+                        <Card sx={{ flexBasis: 200 }}>
+                            <CardContent sx={{height: "100%"}}>
+                                <Typography variant="h3" sx={{ fontSize: 18 }}>
+                                    Adicionar nova meta
+                                </Typography>
+                                <div className="add-meta-div">
+                                    <Button sx={{
+                                        width: "100px",
+                                        height: "100px",
+                                        fontSize: 80
+                                    }}><i className="bi bi-plus"></i></Button>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </div>
+
+                    <div className="mapa-preview">
+                        <h3>Pré-visualização do mapa</h3>
+
+                        <div className="timeline-div">
+                            <Timeline goals={goals}/>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <MenuMobile/>
