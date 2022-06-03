bf62bdb9b9a5efcdc840b9d78f48f4ce9a8c4d70
Natan Barbosa <nerenatan@gmail.com>
2022-06-03 14:35:41 -0300

    criado componente para o botõ de apadrinhar

--- src/componets/layout/navPerfil/NavPerfil.jsx
+++ src/componets/layout/navPerfil/NavPerfil.jsx
@@ -1,12 +1,13 @@
 import "./NavPerfil.css";
-import React from "react";
+import React, {useState} from "react";
 import foto from "../../../media/profile_pictures/natan_proa.jpg";
 import ProfPicture from "../profPicture/ProfPicture";
-import ApadrinharBtn from "../../../media/icons/icons_apadrinhar_icon_fill.svg";
 import ChatBtn from "../../../media/icons/item_chat_icon_fill.svg";
 import ConectionBtn from "../../../media/icons/item_conections_icon_fill.svg";
 import { Link } from "react-router-dom";
 import Cover from "./Valentina_Profile/cover_space.png"
+import ButtonApoiar from "../buttonApoiar/ButtonApoiar";
+import ApadPopup from "../apadPopup/ApadPopup";
 
 const NavPerfil = props => {
     let url = window.location.href
@@ -14,74 +15,87 @@ const NavPerfil = props => {
 
     let profileTab = url[4] || 'publicacoes';
 
+    const[buttonPopup, setButtonPopup] = useState(false);
+
+    if (buttonPopup){
+        document.querySelector('body').style.overflowY = 'hidden';
+    } else {
+        document.querySelector('body').style.overflowY = 'auto';
+    }
+
     return (
-        <div className="NavPerfil">
-            <div className="perfilContainer">
-                <img className="bg_blur_perfil" src={Cover}></img>
-                <div className="perfilHeader">
-                    <div className="header-content">
-                        <div className="perfilCapa">
-                            <img src={Cover}></img>
-                        </div>
-
-                        <div className="perfilNomeInteracao ">
-
-                            <div className="perfilFoto">
-                                <ProfPicture src="natan_proa.jpg" scale={"100%"} />
+        <>
+            <div className="NavPerfil">
+                <div className="perfilContainer">
+                    <img className="bg_blur_perfil" src={Cover}></img>
+                    <div className="perfilHeader">
+                        <div className="header-content">
+                            <div className="perfilCapa">
+                                <img src={Cover}></img>
                             </div>
 
-                            <div className="userName">
-                                <h2>Valentina</h2>
-                                <button className="btn-editar-perfil">
-                                    <i className="bi bi-pencil-square"></i>
-                                    Editar perfil
-                                </button>
-                            </div>
+                            <div className="perfilNomeInteracao ">
 
-                            <div className="buttonsChatConecApad">
-                                <div className="buttonsChat-box">
-                                    <Link to="/chat"><button className="buttonChat"><img src={ChatBtn} alt="" /></button></Link>
+                                <div className="perfilFoto">
+                                    <ProfPicture src="natan_proa.jpg" scale={"100%"} />
                                 </div>
 
-                                <div className="buttonsConecApad">
-                                    <button className="buttonConectar"><img src={ConectionBtn} alt="ícone de apadrinhar" /> Conectar</button>
+                                <div className="userName">
+                                    <h2>Valentina</h2>
+                                    <button className="btn-editar-perfil">
+                                        <i className="bi bi-pencil-square"></i>
+                                        Editar perfil
+                                    </button>
+                                </div>
+
+                                <div className="buttonsChatConecApad">
+                                    <div className="buttonsChat-box">
+                                        <Link to="/chat"><button className="buttonChat"><img src={ChatBtn} alt="" /></button></Link>
+                                    </div>
+
+                                    <div className="buttonsConecApad">
+                                        <button className="buttonConectar"><img src={ConectionBtn} alt="ícone de apadrinhar" /> Conectar</button>
+                                        {
+                                            props.profType === "jovem" ?
+                                                <ButtonApoiar setButtonPopup={setButtonPopup} />
+                                                :
+                                                ""
+                                        }
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <nav className="perfilPubMomMapCfa">
+                                <ul className="perfilPubMomMapCfa">
                                     {
                                         props.profType === "jovem" ?
-                                            <button className="buttonApadrinhar" onClick={() => props.setButtonPopup(true)}><img src={ApadrinharBtn} alt="ícone de " /> Apadrinhar</button>
+                                            <>
+                                                <li><Link to="/perfil/publicacoes"><button>Publicações</button></Link> {profileTab === "publicacoes" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/moments"><button>Moments</button></Link> {profileTab === "moments" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/mapacarreira"><button>Mapa de carreira</button></Link> {profileTab === "mapacarreira" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/cursosrecomendados"><button>Cursos recomendados</button></Link> {profileTab === "cursosrecomendados" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/cursosfavoritos"><button>Cursos favoritos</button></Link> {profileTab === "cursosfavoritos" ? <div className="active"></div> : ""}</li>
+                                            </>
                                             :
-                                            ""
+                                            <>
+                                                <li><Link to="/perfil/nivelpadrinho"><button>Seu nível</button></Link>{profileTab === "nivelpadrinho" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/publicacoes"><button>Publicações</button></Link>{profileTab === "publicacoes" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/historico"><button>Histórico de ajudas</button></Link>{profileTab === "historico" ? <div className="active"></div> : ""}</li>
+                                                <li><Link to="/perfil/apadrinhamentos"><button>Jovens Apadrinhados</button></Link>{profileTab === "apadrinhamentos" ? <div className="active"></div> : ""}</li>
+                                            </>
                                     }
-                                </div>
-                            </div>
 
+                                </ul>
+                            </nav>
                         </div>
-
-                        <nav className="perfilPubMomMapCfa">
-                            <ul className="perfilPubMomMapCfa">
-                                {
-                                    props.profType === "jovem" ?
-                                        <>
-                                            <li><Link to="/perfil/publicacoes"><button>Publicações</button></Link> {profileTab === "publicacoes" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/moments"><button>Moments</button></Link> {profileTab === "moments" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/mapacarreira"><button>Mapa de carreira</button></Link> {profileTab === "mapacarreira" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/cursosrecomendados"><button>Cursos recomendados</button></Link> {profileTab === "cursosrecomendados" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/cursosfavoritos"><button>Cursos favoritos</button></Link> {profileTab === "cursosfavoritos" ? <div className="active"></div> : ""}</li>
-                                        </>
-                                        :
-                                        <>
-                                            <li><Link to="/perfil/nivelpadrinho"><button>Seu nível</button></Link>{profileTab === "nivelpadrinho" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/publicacoes"><button>Publicações</button></Link>{profileTab === "publicacoes" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/historico"><button>Histórico de ajudas</button></Link>{profileTab === "historico" ? <div className="active"></div> : ""}</li>
-                                            <li><Link to="/perfil/apadrinhamentos"><button>Jovens Apadrinhados</button></Link>{profileTab === "apadrinhamentos" ? <div className="active"></div> : ""}</li>
-                                        </>
-                                }
-
-                            </ul>
-                        </nav>
                     </div>
                 </div>
             </div>
-        </div>
+            <ApadPopup
+            trigger={buttonPopup}
+            setTrigger={setButtonPopup}/>
+        </>
     );
 }
 
--- src/componets/layout/post/Post.jsx
+++ src/componets/layout/post/Post.jsx
@@ -2,6 +2,7 @@ import "./Post.css";
 import React, { useState } from "react";
 import ProfPicture from "../profPicture/ProfPicture";
 import getPostDate from "../../funcional/getPostDate";
+import ButtonApoiar from "../buttonApoiar/ButtonApoiar";
 
 //import dos ícones
 import likeIcoFill from "../../../media/icons/item_like_icon_fill.svg";
@@ -10,43 +11,57 @@ import commentIcoFill from "../../../media/icons/item_comment_icon_fill.svg";
 import commentIcoStroke from "../../../media/icons/item_comment_icon_stroke.svg";
 import shareIco from "../../../media/icons/item_share_icon_stroke.svg";
 import rocketIco from "../../../media/icons/icons_apadrinhar_icon_fill.svg";
+import ApadPopup from "../apadPopup/ApadPopup";
 
 const Post = props => {
-    const [buttonPopup, setButtonPopup] = useState(false);
+
+    const[buttonPopup, setButtonPopup] = useState(false);
+
+    if (buttonPopup){
+        document.querySelector('body').style.overflowY = 'hidden';
+    } else {
+        document.querySelector('body').style.overflowY = 'auto';
+    }
 
     return (
-        <div className="Post">
-            <div className="post-header">
-                <div className="header-prof-picture">
-                    <ProfPicture src={props.profPicture} scale={50} />
+        <>
+            <div className="Post">
+                <div className="post-header">
+                    <div className="header-prof-picture">
+                        <ProfPicture src={props.profPicture} scale={50} />
+                    </div>
+                    <div className="header-prof-info">
+                        <span className="fullname">{props.fullname}</span>
+                        <span className="aspiration">{props.aspiration}</span>
+                        <span className="postDate">{getPostDate(props.postDate)}</span>
+                    </div>
                 </div>
-                <div className="header-prof-info">
-                    <span className="fullname">{props.fullname}</span>
-                    <span className="aspiration">{props.aspiration}</span>
-                    <span className="postDate">{getPostDate(props.postDate)}</span>
+                <div className="post-description">
+                    {props.postDescription}
                 </div>
-            </div>
-            <div className="post-description">
-                {props.postDescription}
-            </div>
-            <div className="post-media">
-                <img src={require(`../../../media/post_media/${props.postMedia}`)} alt="Imagem do post" className="post-img" />
-            </div>
-            <div className="post-engagement">
-                <span className="likes">{props.qntLikes} likes</span> <span className="separatos"></span>
-                <div>
-                    <span className="comments">{props.qntComments} comentários</span> <span className="separatos"></span>
-                    <span className="shared">{props.qntShares} compartilhamentos</span>
+                <div className="post-media">
+                    <img src={require(`../../../media/post_media/${props.postMedia}`)} alt="Imagem do post" className="post-img" />
                 </div>
+                <div className="post-engagement">
+                    <span className="likes">{props.qntLikes} likes</span> <span className="separatos"></span>
+                    <div>
+                        <span className="comments">{props.qntComments} comentários</span> <span className="separatos"></span>
+                        <span className="shared">{props.qntShares} compartilhamentos</span>
+                    </div>
 
+                </div>
+                <div className="post-actionBtns">
+                    <button className="btn btn-like"> <img src={likeIcoStroke} alt="icone de like" className="btn-ico" /> Curtir</button>
+                    <button className="btn btn-comment"><img src={commentIcoStroke} alt="icone de like" className="btn-ico" /> Comentar</button>
+                    <button className="btn btn-share"><img src={shareIco} alt="icone de like" className="btn-ico" /> Compartilhar</button>
+                    <ButtonApoiar setButtonPopup={setButtonPopup} />
+                </div>
             </div>
-            <div className="post-actionBtns">
-                <button className="btn btn-like"> <img src={likeIcoStroke} alt="icone de like" className="btn-ico" /> Curtir</button>
-                <button className="btn btn-comment"><img src={commentIcoStroke} alt="icone de like" className="btn-ico" /> Comentar</button>
-                <button className="btn btn-share"><img src={shareIco} alt="icone de like" className="btn-ico" /> Compartilhar</button>
-                <button className="btn btn-apadrinhar" onClick={() => props.setButtonPopup(true)}><img src={rocketIco} alt="icone de like" className="btn-ico" /> Apadrinhar</button>
-            </div>
-        </div>
+
+            <ApadPopup
+                trigger={buttonPopup}
+                setTrigger={setButtonPopup}/>
+        </>
     );
 }
 
