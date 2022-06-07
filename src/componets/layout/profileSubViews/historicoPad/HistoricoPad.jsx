9cfe51b29d17844a414297544973676f7d803ae5
Natan Barbosa <nerenatan@gmail.com>
2022-06-07 19:28:14 -0300

    feito o histórico de ajudas do padrinho

--- src/componets/layout/profileSubViews/historicoPad/HistoricoPad.jsx
+++ src/componets/layout/profileSubViews/historicoPad/HistoricoPad.jsx
@@ -1,10 +1,108 @@
 import "./HistoricoPad.css";
 import React from "react";
+import Ajuda from "./ajuda/Ajuda";
 
 const HistoricoPad = props => {
     return(
         <div className="HistoricoPad">
             <h2>Histórico de ajudas</h2>
+            <div className="ajudas my-scrollbar">
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+
+                <Ajuda
+                    tipo="recomendacao"
+                    padrinho="Natan"
+                    afiliado="Valentina"
+                    data="2022-07-06 19:19"
+                    curso="Programação web para iniciantes"
+                />
+            </div>
         </div>
     );
 }
---
+++ src/componets/layout/profileSubViews/historicoPad/ajuda/Ajuda.jsx
@@ -0,0 +1,40 @@
+import "./Ajuda.css";
+import React from "react";
+import ProfPicture from "../../../profPicture/ProfPicture";
+import If from "../../../../funcional/If";
+import GetPostDate from "../../../../funcional/getPostDate";
+
+const Ajuda = props => {
+    return(
+        <div className="Ajuda">
+            <div className="picture-to-picture">
+                <ProfPicture src="natan_proa.jpg" scale={40}/>
+                <i className="bi bi-arrow-right"></i>
+                <ProfPicture src="valentina.png" scale={40}/>
+            </div>
+            <div className="ajuda-descricao">
+                <div className="descricao">
+                    <If test={props.tipo === "recomendacao"}>
+                        <span><strong>{props.padrinho}</strong> recomendo o curso <strong>{props.curso}</strong> para <strong>{props.afiliado}</strong></span>
+                    </If>
+
+                    <If test={props.tipo === "doacao_dinheiro"}>
+                        <span><strong>{props.padrinho}</strong> doou R$<strong>{props.dinheiro}</strong> para <strong>{props.afiliado}</strong></span>
+                    </If>
+
+                    <If test={props.tipo === "doacao_material"}>
+                        <span><strong>{props.padrinho}</strong> doou <strong>{props.material}</strong> para <strong>{props.afiliado}</strong></span>
+                    </If>
+
+                    <If test={props.tipo === "apadrinhamento"}>
+                        <span><strong>{props.padrinho}</strong> apadrinhou <strong>{props.afiliado}</strong></span>
+                    </If>
+                </div>
+                <div className="data">
+                    {GetPostDate(props.data)}
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Ajuda;
