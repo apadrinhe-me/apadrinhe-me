7d91b45d05495456c2b8491e24bee3402b2d2322
Luis Gustavo Almeida Reis <luisgustavoareis@gmail.com>
2022-06-09 22:09:44 -0300

    Estilização da tela de cadastro

--- src/views/pages/Cadastro.jsx
+++ src/views/pages/Cadastro.jsx
@@ -1,11 +1,28 @@
 import "./Cadastro.css";
 import React from "react";
+import Estilo2 from "../../media/Imagens_pop-up/estilodir_cadastro.svg"
+import Estilo1 from "../../media/Imagens_pop-up/estiloesq_cadastro.svg"
 
- const Cadastro = () => {
-    return(
-        <div className="Cadastro">
-            <h1>Cadastro</h1>
-        </div>
+const Cadastro = () => {
+    return (<div className="Cadastro">
+
+
+        <div className="Cards">
+            <div className="cardPadrinho"></div>
+            <div className="cardAfiliado"></div>
+            <img className="estilo2" src={Estilo2} />
+            <img className="estilo1" src={Estilo1} />
+
+        </div><div className="estiloesq"></div>
+
+
+
+
+
+
+
+
+    </div>
     );
 }
 
