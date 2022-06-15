8c12b877b2af151aef4a263ac2fcaa09270c4fa4
Luis Gustavo Almeida Reis <luisgustavoareis@gmail.com>
2022-06-15 18:09:49 -0300

    Estilização da tela de cadastro

--- src/views/pages/Cadastro.jsx
+++ src/views/pages/Cadastro.jsx
@@ -1,13 +1,17 @@
 import "./Cadastro.css";
 import React, { useState, useRef } from "react";
-import Logo from "../../media/logo/logoredondo-03.svg";
 import Card_cadastro from "../../componets/layout/card_cadastro/Card_cadastro";
 
 
 
 const Cadastro = () => {
+
     const ser_apadrinhado = useRef()
-    console.log(ser_apadrinhado)
+    const apadrinhar = useRef()
+    console.log(ser_apadrinhado, apadrinhar)
+
+
+
     return (<div className="Cadastro">
         <div className="Cards" >
             <div className="container_cards">
@@ -15,7 +19,7 @@ const Cadastro = () => {
                     <h3 className="title_card">Quero ser <br /> afilhado(a)</h3>
                     <p className="text_card">Crie conteúdeo para ser visto e receber ajuda de diversos apoiadores cadastrados na plataforma</p>
                 </Card_cadastro>
-                <Card_cadastro type="apadrinhar">
+                <Card_cadastro type="apadrinhar" ref={apadrinhar}>
                     <h3 className="title_card">Quero <br />apoiar</h3>
                     <p className="text_card">  Acompanhe o desenvolvimento de jovens e encontre um para apoiar de diversas maneiras  </p>
                 </Card_cadastro>
