b162ffc14e1d359cf858c7e6deda2d6d1ec582eb
Luis Gustavo Almeida Reis <luisgustavoareis@gmail.com>
2022-06-15 18:16:39 -0300

    Criação Componente segunda etapa do cadastro

---
+++ src/componets/layout/pagesCadastro/Cadastro_jovem1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Cadastro_jovem1.css"
+
+const Cadastro_jovem1 = () => {
+
+    return (
+        <div>TESTE</div>
+    )
+}
+export default Cadastro_jovem1
--- src/views/pages/Cadastro.jsx
+++ src/views/pages/Cadastro.jsx
@@ -1,6 +1,7 @@
 import "./Cadastro.css";
 import React, { useState, useRef } from "react";
 import Card_cadastro from "../../componets/layout/card_cadastro/Card_cadastro";
+import Cadastro_jovem1 from "../../componets/layout/pagesCadastro/Cadastro_jovem1";
 
 
 
@@ -15,14 +16,16 @@ const Cadastro = () => {
     return (<div className="Cadastro">
         <div className="Cards" >
             <div className="container_cards">
-                <Card_cadastro type="ser_apadrinhado" ref={ser_apadrinhado}>
+                {/*<Card_cadastro type="ser_apadrinhado" ref={ser_apadrinhado}>
                     <h3 className="title_card">Quero ser <br /> afilhado(a)</h3>
                     <p className="text_card">Crie conteúdeo para ser visto e receber ajuda de diversos apoiadores cadastrados na plataforma</p>
                 </Card_cadastro>
                 <Card_cadastro type="apadrinhar" ref={apadrinhar}>
                     <h3 className="title_card">Quero <br />apoiar</h3>
                     <p className="text_card">  Acompanhe o desenvolvimento de jovens e encontre um para apoiar de diversas maneiras  </p>
-                </Card_cadastro>
+                </Card_cadastro>*/}
+                <Cadastro_jovem1 />
+
             </div>
         </div>
 
