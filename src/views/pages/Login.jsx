847c3eb2afa99eeef732b822d09c4396de8e3aaf
Natan Barbosa <nerenatan@gmail.com>
2022-06-16 14:57:06 -0300

    alteraaações de estilo na página de login

--- src/views/pages/Login.jsx
+++ src/views/pages/Login.jsx
@@ -2,23 +2,23 @@ import "./Login.css"
 import React from "react";
 import Logo from "../../media/logo/logoredondo-03.svg"
 import apadrinheme from "../../media/logo/apadrinhe-04.svg"
-
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
         <div className="Login">
             <div></div>
             <div className="principal_box">
-                <div><a className="voltar" href="/">x</a></div>
+                <div className="voltar-div"><Link className="voltar" to="/"><i className="bi bi-arrow-return-left"></i> Voltar para home</Link></div>
                 <div className="logo_box"><img className="logo" src={Logo} /></div>
 
                 <div className="logo_apadrinheme"><img className="logoescrito" src={apadrinheme} width="45px" /></div>
 
-                <input autocaptalize="none" placeholder="Usuário" type="text" className='user_input'></input>
-                <input autocaptalize="none" placeholder="Senha" type="password" maxLength={8} className='password_input'></input>
-                <p className="p1">Esqueci minha senha</p>
+                <input placeholder="Usuário" type="text" className='user_input'></input>
+                <input placeholder="Senha" type="password" maxLength={8} className='password_input'></input>
+                <span className="p1">Esqueci minha senha</span>
                 <button>Entrar</button>
-                <p className="p2">Não tem conta? <strong>Cadastre-se</strong></p>
+                <p className="p2">Não tem conta? <Link to="/cadastro">Cadastre-se</Link></p>
 
             </div>
 
