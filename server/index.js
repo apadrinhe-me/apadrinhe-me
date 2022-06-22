9b0d7a3eed214a741f7ae7b65d7a049a7a798a78
Cristian ferreira <cristianferreiradeoliveira.ti@gmail.com>
2022-06-22 10:45:26 -0300

    Rsolvendo bugs da pasta serve

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql1");
+const mysql = require("mysql");
 
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "passoword",
-    database: "banco",
+    password: "root",
+    database: "apadrinhe_me",
 });
 
 app.get('/', (req, res) => {
-    db.query("INSERT INTO usuarios (email, senha) VALUES( 'cristianferreira@gmail.com', '1234567')")
-})
+    db.query(
+        "INSERT INTO usuarios ( email_usuario, senha_usuario, nome_completo_usuario, nickname_usuario, data_nasc_usuario, genero_usuario, confirmacao_responsavel, confirmacao_usuario, renda_familiar) VALUES('cristianferreira@gmail.com', '1234567', 'cris', 'cris123', '08/03/2003', 'masculino','sim', 'sim', '1000')", (err, result) => {if(err) {console.log(err)}
+    }
+    );
+});
 
 
 app.listen(3001, () => {
     console.log("rodando na portan 3001");
-});1
+}); 
